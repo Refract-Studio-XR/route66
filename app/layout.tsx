045bc8d4fabcc10b80bed7fe2d4a3641130cc0f4
Sam Jones <bsamjones@gmail.com>
2025-09-04 17:17:08 -0600

    common bb

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <script
+        {/* <script
           dangerouslySetInnerHTML={{
             __html: `
               (function setAppHeight() {
@@ -30,7 +30,7 @@ export default function RootLayout({
               })();
             `,
           }}
-        />
+        /> */}
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, viewport-fit=cover"
