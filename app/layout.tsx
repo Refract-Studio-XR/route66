e8266acbbe11f532cbcadbeced0f65a9cee6de24
Sam Jones <bsamjones@gmail.com>
2025-09-04 17:13:01 -0600

    boo

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,14 @@ export default function RootLayout({
           name="viewport"
           content="width=device-width, initial-scale=1.0, viewport-fit=cover"
         />
+        <meta
+          name="apple-mobile-web-app-capable"
+          content="yes"
+        />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
       </head>
 
       <body className={`${inter.className} h-app`}>{children}</body>
